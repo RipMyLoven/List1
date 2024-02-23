4edb775a51d79c7e5b9dcf676e8453a0ab816e51
RipMyLoven <130289194+RipMyLoven@users.noreply.github.com>
2024-02-23 08:34:41 +0200

    Update iseseisevtöö2.py

--- iseseisevtöö2.py
+++ iseseisevtöö2.py
@@ -1,15 +1,13 @@
 ﻿import random
 import time
 
-raha = 100  # Alguses on mängijal 100 eurot
+raha = 100  
 
 print("Tere tulemast kasiinosse!")
 print("Siin kasiinos on mäng, kus võitmine on võimatu.")
 
-while raha >= 10:  # Mäng jätkub seni, kuni mängijal on vähemalt 10 eurot
-
+while raha >= 10:
     print(f"\nTeil on hetkel {raha} eurot.")
-
     while True:
         try:
             panus = int(input("Palun sisestage oma panus (vähemalt 10 eurot): "))
@@ -20,14 +18,10 @@ while raha >= 10:  # Mäng jätkub seni, kuni mängijal on vähemalt 10 eurot
             break
         except ValueError as ve:
             print(ve)
-
+            
     print("Pöörutan ratast...")
-    time.sleep(1)  # Ootame 1 sekund enne tulemuse kuvamist
-
-    # Simuleerime ratta pööramist
+    time.sleep(1)  
     varv = random.randint(1, 10)
-
-    # Näitame ratta pöörlemist
     for frame_number in range(5):
         if frame_number == 0:
             print("|   |   |   |", end="\r")
@@ -39,23 +33,20 @@ while raha >= 10:  # Mäng jätkub seni, kuni mängijal on vähemalt 10 eurot
             print("| {} | {} | {} |".format(varv, varv, varv), end="\r")
         elif frame_number == 4:
             print("| {} | {} | {} |".format(varv, varv, varv), end="\r")
-
-        time.sleep(0.5)  # Ootame enne järgmise kaadri kuvamist
-
-    # Näitame tulemust järk-järgult
+        time.sleep(0.5)  
+        
     print("\nTeie number on:", end=" ")
     for i in range(1, varv + 1):
         print(i, end=" ", flush=True)
         time.sleep(0.5)
+        
     print()
 
-    # Mängijaraha uuendamine vastavalt tulemusele
     if varv == 10:
         print("Te võitsite!")
         raha += panus
     else:
         print("Te kaotasite!")
-        # Simuleerime juhuslikud numbrid enne lõpliku tulemuse kuvamist
         for i in range(varv + 1, 10):
             print(i, end=" ", flush=True)
             time.sleep(0.5)
